eb9baf931e45b94e558c0814ecd5591ec5b957cb
Wes Widner <kai5263499@gmail.com>
2015-09-16 08:14:33 -0400

    remove console.log

--- tests/basic.js
+++ tests/basic.js
@@ -2,8 +2,6 @@ var assert        = require("assert");
 var should        = require("should");
 var SuddenStats   = require("../SuddenStats");
 
-console.log(SuddenStats);
-
 describe('basic SuddenStats instance', function () {
  it('should return an object', function (done) {
     var suddenStats = new SuddenStats();
