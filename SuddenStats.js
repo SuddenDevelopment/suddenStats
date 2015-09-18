24ef01375c4fcab11012336b5f165b389acc7671
SuddenDevelopment <anthony.aragues@gmail.com>
2015-09-18 12:15:57 -0700

    debugged enough for basic function to pass tests :)

--- SuddenStats.js
+++ SuddenStats.js
@@ -26,13 +26,13 @@ objConfig={
 var _ = require('lodash');
 var SuddenStats = function(objConfig){
 	//start with some defaults, the global stat will require nuothing but a number passed in an array.
-	this.config = {limit:10000,throttle:100,stats:{primary:{type:'numeric'}}}
+	this.config = {limit:10000,throttle:100,stats:{primary:{type:'numeric'}}};
 	//simple mode is when only the global stat is used and only arrays of numbers are passed in. by setting this once for the object it avoids many typechecks
 	this.simple = false;
 	this.stats = {};
 	this.batch = [];
 	this.intBatch = 0;
-	// this.init(objConfig);
+	var self=this;
 
 	this.init = function(objConfig){
 		//TODO: validate the structure of config passed in
@@ -42,26 +42,28 @@ var SuddenStats = function(objConfig){
 		//create all of the stats properties for what will be tracked
 		_.forOwn(this.config.stats,function(objV,k){
 			//set defaults for stat
-			this.stats[k] = {min:0,max:0,avg:0,count:0,total:0,first:false,last:false,lastAvg:false,diff:0,fs:Date.now(),ls:Date.now(),type:'numeric'};
+			self.stats[k] = {min:0,max:0,avg:0,count:0,total:0,first:false,last:false,lastAvg:false,diff:0,fs:Date.now(),ls:Date.now(),type:'numeric'};
 			//add any user overrides to things like type
-			_.merge(this.stats[k],objV);
+			_.merge(self.stats[k],objV);
 		});
 		//can we go into simple mode? if so it will be much faster, and generic of course
-		if(_.keys(this.stats)==1 && this.stats.hasOwnProperty('primary')){ this.simple = true; }
+		if(self.stats.primary){ self.simple = true; }
 	}
+
+	this.init(objConfig);
 
 	this.qData = function(varData){
 		//add data as often as you want, but batch it by time, using the deboucne config, defined in miliseconds
 		//config.limit is to start kciking things out of the batch after too many per batch / time period
-		if(this.intBatch<this.config.limit){
-			this.batch.push(varData);
+		if(self.intBatch<this.config.limit){
+			self.batch.push(varData);
 			/* 
 			if(fnInprocess){ 
-				if(this.config.limit > 2){this.config.limit--;} 
-				this.config.throttle++;
+				if(self.config.limit > 2){self.config.limit--;} 
+				self.config.throttle++;
 			}
 			*/
-			var fnInprocess = _.throttle(this.addData(this.batch),this.config.throttle);
+			var fnInprocess = _.throttle(self.addData(self.batch),self.config.throttle);
 		}
 	}
 
@@ -69,49 +71,50 @@ var SuddenStats = function(objConfig){
 		//always work with an array of data. Data is expected be in such vlume that it should be buffered into batches
 		
 		//clear the batch
-		this.batch=[]; this.intBatch=0;
+		self.batch=[]; self.intBatch=0;
 
-		if(this.simple){ this.updateStat(arrData,'primary'); }
-		else{ this.addCustomData(arrData);
+		if(self.simple){ self.updateStat(arrData,'primary'); }
+		else{
 			var arrBatch={};
-				_.forOwn(this.config.stats,function(objStat,strStat){
+				_.forOwn(self.config.stats,function(objStat,strStat){
 					//init the batch so all we have to do is push.
-					arrBatch[strStat].data = [];
+					arrBatch[strStat]={data:[]};
 				});
 			//data loop
 			_.forEach(arrData,function(objData,k){
 				//stats loop
-				_.forOwn(this.config.stats,function(objStat,strStat){
+				_.forOwn(self.config.stats,function(objStat,strStat){
 					//get the numbers from the object based on the path
 					arrBatch[strStat].data.push( _.get(objData,objStat.path) );
 				});
 			});
 			_.forOwn(arrBatch,function(objStat,strStat){
 				//init the batch so all we have to do is push.
-				this.updateStat(objStat.data,arrBatch[strStat]);
+				self.updateStat(objStat.data,arrBatch[strStat]);
 			});
 		}
-		return this.stats;
+		console.log(self.stats);
 	};
 
 	this.updateStat = function(arrData,key){
 		//these are light, but do them as little as necessary
+		//console.log(this.stats);
 		var intMin = _.min(arrData);
 		var intMax = _.max(arrData);
 		var intSum = _.sum(arrData);
 		var intCount = arrData.length;
 		var intAvg = intSum/intCount;
-		if( intMin < this.stats[key].min ){ this.stats[key].min = intMin; }
-		if( intMax > this.stats[key] ){ this.stats[key].max = intMax; }
-		this.stats[key].count = this.stats[key].count + intCount;
-		this.stats[key].total = this.stats[key].total + intSum;
-		this.stats[key].last = _.last(arrData);
-		if( this.stats[key].first===false ){ this.stats[key].first = _.first(arrData); this.stats[key].fs=Date.now(); }
-		this.stats[key].ls=Date.now();
-		this.stats[key].avg=this.stats[key].total/this.stats[key].count;
-		this.stats[key].diff = intAvg-this.stats[key].lastAvg;
-		this.stats[key].lastAvg = intAvg;
-	}
+		if( intMin < self.stats[key].min || self.stats[key].first===false ){ self.stats[key].min = intMin; }
+		if( intMax > self.stats[key].max ){ self.stats[key].max = intMax; }
+		self.stats[key].count = self.stats[key].count + intCount;
+		self.stats[key].total = self.stats[key].total + intSum;
+		self.stats[key].last = _.last(arrData);
+		if( self.stats[key].first===false ){ self.stats[key].first = _.first(arrData); self.stats[key].fs=Date.now(); }
+		self.stats[key].ls=Date.now();
+		self.stats[key].avg=self.stats[key].total/self.stats[key].count;
+		self.stats[key].diff = intAvg-self.stats[key].lastAvg;
+		self.stats[key].lastAvg = intAvg;
+			}
 
 };
 module.exports = SuddenStats;
--- tests/basic.js
+++ tests/basic.js
@@ -4,8 +4,22 @@ var SuddenStats   = require("../SuddenStats");
 
 describe('basic SuddenStats instance', function () {
  it('should return an object', function (done) {
-    var suddenStats = new SuddenStats();
-   (typeof suddenStats).should.equal('object');
+    var objStats = new SuddenStats();
+   (typeof objStats).should.equal('object');
+   done();
+ });
+});
+
+describe('basic SuddenStats instance 2', function () {
+ it('should return max=6', function (done) {
+    //test
+
+
+    var objStats = new SuddenStats();
+    objStats.addData([1,2,3,4,5,6]);
+    
+    //check
+   (objStats.stats.primary.max).should.be.exactly(6);
    done();
  });
 });
